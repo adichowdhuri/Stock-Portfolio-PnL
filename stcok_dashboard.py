14e8bbde5618255ade599c9229eceaa0b200ee8f
adichowdhuri <adichowdhuri@gmail.com>
2025-08-31 11:33:29 +0530

    added requirements

--- stcok_dashboard.py
+++ stcok_dashboard.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import yfinance as yf
-from datetime import datetime, timedelta
+import datetime as date
 
 st.title("Portfolio PnL Dashboard")
 st.set_page_config(page_title="Portfolio PnL", page_icon="📈")
