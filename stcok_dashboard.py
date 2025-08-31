184287730cc992d539ff534b7d079bf267c4e9da
adichowdhuri <adichowdhuri@gmail.com>
2025-08-31 12:37:49 +0530

    stock to data frame fix

--- stcok_dashboard.py
+++ stcok_dashboard.py
@@ -16,13 +16,12 @@ st.header("Add to Portfolio")
 ticker = st.text_input("Stock Ticker (e.g., AAPL)")
 buy_date = st.date_input("Buy Date", date(2023, 2, 1))
 quantity = st.number_input("Quantity", 1, step=1)
-
+ticker = "AAPL"
 default_price = 0.0
 if ticker and buy_date:
     try:
         data = yf.download(ticker, start=buy_date, end=buy_date + timedelta(days=1), progress=False)
         if not data.empty:
-            print(data)
             default_price = float(round(data["Open"], 2).iloc[0].iloc[0])
     except Exception as e:
         st.warning(f"Could not fetch price: {e}")
@@ -33,7 +32,7 @@ buy_price = st.number_input("Buy Price", value=default_price)
 if st.button("Add to Portfolio"):
     if ticker and buy_price:
         new_row = pd.DataFrame({"Ticker": [ticker], "Buy Date": [buy_date], "Buy Price": [buy_price], "Quantity": [quantity]})
-        st.session_state.portfolio = st.session_state.portfolio.append(new_row, ignore_index=True)
+        st.session_state.portfolio = st.session_state.portfolio.concat(new_row, ignore_index=True)
 
 # Show current portfolio
 st.subheader("Current Portfolio")
